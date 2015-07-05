1d95405a3aa8b0118032e9dcca574fdc6efa9904
Casey Ydenberg <ydenberg@gmail.com>
2015-07-05 17:25:22 -0600

    Simplified routes

--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,26 @@ router.get('/lens', function(req, res) {
   res.render('lens');
 });
 
-
 /* GET other pages. */
 router.all('/*', function(req, res, next) {
 	req.context = {};
   if (env === 'development') {
-		req.context.stylesheets = ['vendor.css', 'style.css'];
+		req.context.stylesheets = ['style.css'];
 		req.context.scripts = ['vendor.js', 'script.js'];
 	} else {
-		req.context.stylesheets = ['vendor.min.css', 'style.min.css'];
+		req.context.stylesheets = ['style.min.css'];
 		req.context.scripts = ['vendor.min.js', 'script.min.js'];
 	}
 	next();
 });
 
-
 router.get('/', function(req, res) {
+  req.context.pagename = 'home';
   res.render('home', req.context);
 });
 
 router.get('/search', function(req, res) {
+  req.context.pagename = 'search';
   res.render('search', req.context);
 });
 
@@ -41,5 +41,6 @@ router.get('/signup', function(req, res) {
 router.get('/signin', function(req, res) {
 	res.render('signin', req.context);
 });
+
 
 module.exports = router;
