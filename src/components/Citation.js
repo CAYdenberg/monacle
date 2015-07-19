37be76b0a6b77d038d541e10e803aff9925d9614
Casey Ydenberg <ydenberg@gmail.com>
2015-07-19 14:43:23 -0600

    Formatting author list to a single string

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -1,11 +1,23 @@
+var _ = require('underscore');
 var React = require('react');
 
 module.exports = React.createClass({
+  formatAuthorList : function() {
+    var authArr,
+      authStr = '';
+    if (this.props.data.authors) {
+      authArr = _.map(this.props.data.authors, function(author) {
+        return author.name;
+      });
+      authStr = authArr.join(', ');
+    }
+    return authStr;
+  },
   render: function() {
     return (
       <div className="item">
         <h4>{this.props.data.title}</h4>
-        <h5>{this.props.data.author}</h5>
+        <h5>{this.formatAuthorList()}</h5>
       </div>
     )
   }
