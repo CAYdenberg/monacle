f5d4ebf72bc67c867baaa6dfb1087ef5e79232e3
Casey Ydenberg <ydenberg@gmail.com>
2015-09-14 21:35:39 -0600

    Added validation functions

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var _ = require('underscore');
 var popsicle = require('popsicle');
+var validateEmail = require('validate-email');
 
 var utils = require('../utils');
 var dispatcher = utils.dispatcher;
 var emitter = utils.emitter;
 var createTypingCallback = utils.createTypingCallback;
+
 
 module.exports = function(store) {
 
@@ -16,38 +18,92 @@ module.exports = function(store) {
         password1: '',
         password2: '',
         emailErr: '',
-        passwordErr: ''
+        password1Err: '',
+        password2Err: ''
       });
     },
+
+    //validation functions
+    isEmailValid: function() {
+      if (validateEmail(this.state.email)) {
+
+      }
+    },
+    isPasswordValid: function() {
+
+    },
+    checkPasswordsMatch: function() {
+      var password1 = this.state.password1;
+      var password2 = this.state.password2;
+      //replace with more complex validation later
+      if ( password1 === password2 ) {
+        this.setState({ password2Err: '' });
+        return true;
+      } else {
+        this.setState({ password2Err: 'Passwords do not match' });
+        return false;
+      }
+    },
+
     submit: function(e) {
       e.preventDefault();
       //check that passwords match
-      dispatcher.dispatch({
-        type: 'CREATE_USER',
-        content: {
-          email: this.state.email,
-          password: this.state.password
-        }
-      });
+      if ( this.checkPasswordsMatch() ) {
+        dispatcher.dispatch({
+          type: 'CREATE_USER',
+          content: {
+            email: this.state.email,
+            password: this.state.password1
+          }
+        });
+      }
     },
+
+    reset: function(stateKeyToReset) {
+      var o = this;
+      return function() {
+        setStateArgs = {};
+        setStateArgs[stateKeyToReset] = '';
+        o.setState(setStateArgs);
+      }
+    },
+    password2Blur: function() {
+      this.checkPasswordsMatch();
+    },
+
     render: function() {
       return (
-        <form method="POST" action="/user/signup/">
+        <form>
           <div className="form-group">
             <label htmlFor="su-email">Email address</label>
-            <input type="email" className="form-control" id="su-email" name="email" onChange={createTypingCallback('email', this)} />
+            <input type="email" className="form-control" id="su-email" name="email" value={this.state.email}
+              onChange={createTypingCallback('email', this)}
+              onFocus={this.reset('emailErr')}
+              onBlur={this.checkEmail}
+            />
             <ErrorMsg message={this.state.emailError} type="warning" />
           </div>
           <div className="form-group">
             <label htmlFor="su-password">Password</label>
-            <input type="password" className="form-control" id="su-password" name="password" onChange={createTypingCallback('password', this)} />
+            <input type="password" className="form-control" id="su-password" name="password" value={this.state.password1}
+              onFocus={this.reset('password1Err')}
+              onBlur={this.password1Blur}
+              onChange={createTypingCallback('password1', this)}
+            />
+            <ErrorMsg message={this.state.password1Err} type="warning" />
           </div>
           <div className="form-group">
             <label htmlFor="su-password-2">Repeat Password</label>
-            <input type="password" className="form-control" id="su-password-2" name="password-2" onChange={createTypingCallback('password2', this)} />
-            <ErrorMsg message={this.state.passwordError} type="warning" />
+            <input type="password" className="form-control" id="su-password-2" name="password-2" value={this.state.password2}
+              onFocus={this.reset('password2Err')}
+              onBlur={this.password2Blur}
+              onChange={createTypingCallback('password2', this)}
+            />
+            <ErrorMsg message={this.state.password2Err} type="warning" />
           </div>
-          <input type="submit" value="Submit" className="btn btn-success" />
+          <input type="submit" value="Submit" className="btn btn-success"
+            onClick={this.submit}
+          />
         </form>
       )
     }
