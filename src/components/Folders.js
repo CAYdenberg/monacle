e3278c762825005694b07f333f84b0a8f95bfd4f
Casey Ydenberg <ydenberg@gmail.com>
2015-10-22 19:52:14 -0600

    Restyled nav

--- src/components/Folders.js
+++ src/components/Folders.js
@@ -39,15 +39,17 @@ module.exports = function(store, userStore) {
       if (this.state.loggedIn) {
         return (
           <div>
-            {
-              this.state.folders.map(function(folder) {
-                return (<Folder data={folder} key={folder.slug} />)
-              })
-            }
+            <ul className="folder-list">
+              {
+                this.state.folders.map(function(folder) {
+                  return (<Folder data={folder} key={folder.slug} />)
+                })
+              }
+            </ul>
             <form className="form-inline" method="POST" onSubmit={this.add}>
               <div className="form-group">
-                <input type="text" className="form-control" value={this.state.newFolderName} onChange={createTypingCallback('newFolderName', this)} />
-                <button type="submit">+</button>
+                <input type="text" className="form-control new-folder-input" value={this.state.newFolderName} onChange={createTypingCallback('newFolderName', this)} />
+                <button type="submit" className="btn-new-folder">+</button>
               </div>
             </form>
           </div>
