b9cc6cc3ccbf0155df302c1bc1661b61c5a38b26
Casey Ydenberg <ydenberg@gmail.com>
2016-01-02 13:18:55 -0700

    Rewrote notifier

--- src/utils/notifier.js
+++ src/utils/notifier.js
@@ -1,39 +1,58 @@
+var _ = require('underscore');
 var EE = require('event-emitter');
 
-var alerts = {
-  loginFailed: {
-    type: 'warning',
-    message: 'Your username and password do not match'
-  },
-  lostPubmed: {
-    type: 'danger',
-    message: 'Unable to reach PubMed server at this time.',
-    retry: true
-  },
-  lostBackend: {
-    type: 'danger',
-    message: 'Unable to reach Monocle',
-    retry: true
-  },
-  addedToFolder: {
-    type: 'success',
-    message: 'Paper successfully saved to folder',
-    autodismiss: true
-  }
-};
+var dispatcher = require('../utils/').dispatcher;
+var emitter = EE({});
 
 function Notifier() {
-  EE.call(this);
   this.notifications = [];
 }
 
-Notifier.prototype = Object.create(EE.prototype);
+/**
+ * Create a notification by calling notifier.create({object literal})
+ *
+ * Notifications can have any properties but typically include:
+ * message - message displayed to user
+ * class - BS Alert class (success, info, warning, danger)
+ * payload - a Dispatcher payload which can be re-sent to the dispatcher by calling
+ * notifcation.retry()
+ * autodismiss - Boolean whether to dismiss the notification after 3 sec
+ *
+ * Notifications have two methods:
+ * dismiss (removes the notification from the Notifier)
+ * retry (sends the notifications payload to the dispatcher)
+ *
+ * This Notifier.create returns the notification so these methods can be called
+ */
+Notifier.prototype.create = function(settings) {
+  var defaults = {
+    message: '',
+    class: 'danger',
+    payload: false,
+    autodismiss: false
+  }
+  var notification = _.extend(defaults, settings);
+  var notifier = this;
 
-Notifier.prototype.create = function(alertName, payload) {
-  var notification = new Notification(alertName, payload);
+  //append dismiss method
+  notification.dismiss = function() {
+    this.notifications.splice(notifier.notifications.indexOf(this), 1);
+    emitter.emit('UPDATE');
+  };
+
+  //append retry method
+  notification.retry = function() {
+    if (notification.payload) {
+      dispatcher.dispatch(this.payload);
+      this.dismiss();
+    }
+  }
+
   this.notifications.push(notification);
-  this.emit('NOTIFICATION');
-  if ( notification.alert.autodismiss ) {
+  emitter.emit('UPDATE');
+
+  //dismiss after 3 sec if autodismiss is set to true
+  if (notification.autodismiss) {
     setTimeout(function() {
       notification.dismiss();
     }, 3000);
@@ -41,16 +60,8 @@ Notifier.prototype.create = function(alertName, payload) {
   return notification;
 }
 
-var notifier = new Notifier();
-
-function Notification(alertName, payload) {
-  this.alert = alerts[alertName];
-  this.payload = payload;
-}
-
-Notification.prototype.dismiss = function() {
-  notifier.notifications.splice(notifier.notifications.indexOf(this), 1);
-  this.emit('NOTIFICATION');
+Notifier.prototype.onUpdate = function(callback) {
+  emitter.on('UPDATE', callback);
 }
 
 module.exports = new Notifier();
